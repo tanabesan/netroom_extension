630fd74337bde013f01b7633928733740a49c9be
AAniki <aaniki76099@gmail.com>
2024-09-21 11:06:36 +0900

    ベーススクリプト(試作)追加
    LOADING.htmlはあとでLOADING.jsに組み込む
    LOADING.jsはあとでユーザースクリプト化する
    ダークモード識別は設定でストレージに保存した値優先(にあとでする)設定されてない場合はシステム設定をストレージに保存
    loading_1_b.gifはダークモード用読み込みアニメーション
    loading_1_b.gifはライトモード用読み込みアニメーション
    loading_2.gifはフリー素材使用
    main_style.css,main-script.jsはまだ着手せず
    カラーメモは読み込みの背景の色のメモ(あとで削除)

---
+++ LOADING.js
@@ -0,0 +1,80 @@
+﻿/* 
+NETROOM extensions load program
+made by AAAAAAAAAAAA.
+All rights reserved.
+ */
+
+/*
+htmlタグ変更帯(LOADING)
+*/
+
+//スマホ識別
+function PC() {
+    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//ダークモード対応
+if (localStorage.hasOwnProperty("darkmode")) {
+    var dark = localStorage.getItem("darkmode");
+} else {
+    var dark = "";
+}
+function isDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+if (dark == "") {
+    if (isDarkMode() == true) {
+        dark = "dark";
+        localStorage.setItem('darkmode', 'dark');
+    } else {
+        dark = "light";
+        localStorage.setItem('darkmode', 'light');
+    }
+} else {
+    if (dark == "dark") {
+        dark = "dark";
+    } else {
+        dark = "light";
+    }
+}
+if (localStorage.hasOwnProperty("darkmode")) {
+    var old_sys_dark = localStorage.getItem("sys-darkmode");
+    console.log("old_sys_dark: " + old_sys_dark);
+}
+
+    if (isDarkMode() == true) {
+        var sys_dark = "dark";
+        localStorage.setItem('sys-darkmode', 'dark');
+    } else {
+        var sys_dark = "light";
+        localStorage.setItem('sys-darkmode', 'light');
+    }
+
+if (old_sys_dark) {
+    if (sys_dark == old_sys_dark) {console.log("なにもおきません") } else {
+        alert("前回のダークモード設定と変わってますな");
+        console.log("前回のダークモード設定と変わってますな");//あとでシステムに合わせるか問うプログラムにあとで変える
+    }
+}
+console.log(dark);
+//スマホ対応
+
+if (PC() == true) {
+
+} else {
+
+}
+
+//ベースHTML
+
+let main_html = `
+
+`;
+
+//変更
+
+document.documentElement.innerHTML = main_html;
---
+++ main-script.js
@@ -0,0 +1,5 @@
+/* 
+NETROOM extensions main program
+made by AAAAAAAAAAAA.
+All rights reserved.
+ */

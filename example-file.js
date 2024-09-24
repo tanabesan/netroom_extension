cf5fc40181aa8417f6aaa949a69494c276954967
AAniki <aaniki76099@gmail.com>
2024-09-24 21:38:24 +0900

    プログラム作成のサンプル(テンプレート(仮))を作成 必要に応じてコピーして使う

---
+++ example-file.js
@@ -0,0 +1,18 @@
+/* 
+NETROOM extensions template
+This tool made by 〇〇.
+All rights reserved.
+ */
+
+/*
+ここに発火方法を記 例:
+example_function1(ユーザー名,現在時刻,(コマンド));で発火
+example_function2((引数不要));で発火
+など
+*/
+
+
+//○○する関数
+function example_function1(user, now_time, cmd){
+    //内容省略
+}

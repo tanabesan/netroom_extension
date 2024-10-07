cf023287d286c932d5a3e5e3abddf3d56c2a48ef
田辺 <152053837+tanabesan@users.noreply.github.com>
2024-10-07 09:52:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 window.addEventListener("load", function() {
     const splashScreen = document.getElementById("splash-screen");
-    const logo = document.getElementById("splash-logo");
-
-    // GIFを最初から再生するためにタイムスタンプを追加
-    const originalSrc = logo.src;
-    logo.src = `${originalSrc}?${new Date().getTime()}`;
-
-    // ローディング画面のフェードアウト処理
     setTimeout(function() {
-        splashScreen.style.opacity = "0"; // フェードアウト
-        splashScreen.style.transition = "opacity 0.5s ease"; // アニメーション設定
+        splashScreen.style.opacity = "0";
+        splashScreen.style.transition = "opacity 0.5s ease";
         setTimeout(function() {
-            splashScreen.style.display = "none"; // 0.5秒後に非表示
-        }, 500); // 500ms後に非表示にする
-    }, 1850); // 1850ms後にフェードアウトを開始
+            splashScreen.style.display = "none";
+        }, 500); // 0.5秒後に非表示
+    }, 1850); // 1.85秒後にフェードアウトを開始
 });
-

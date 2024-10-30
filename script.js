dab0445c8c99cae1393e643b509c31a8d5020272
iN / いん <152053837+tanabesan@users.noreply.github.com>
2024-10-30 13:19:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-if (typeof load === "undefined") {
-    var load = 0
-} 
+window.addEventListener("load", function() {
+    let load = sessionStorage.getItem("load");
 
-if (load === 0) {
-    window.addEventListener("load", function() {
+    if (load === null) {
+        load = 0;
         const splashScreen = document.getElementById("splash-screen");
+        splashScreen.style.display = "block"; 
+
         setTimeout(function() {
             splashScreen.style.opacity = "0";
             splashScreen.style.transition = "opacity 0.5s ease";
+
             setTimeout(function() {
                 splashScreen.style.display = "none"; 
+                sessionStorage.setItem("load", 1); 
             }, 500); 
         }, 1850); 
-    });
-    load = 1
-}
-
-
-
+    } else {
+        const splashScreen = document.getElementById("splash-screen");
+        splashScreen.style.display = "none"; 
+    }
+});

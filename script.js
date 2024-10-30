0a7583f9eb1776e828bb53ced7e66cf9963c9dec
iN / いん <152053837+tanabesan@users.noreply.github.com>
2024-10-30 13:12:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-window.addEventListener("load", function() {
-    const splashScreen = document.getElementById("splash-screen");
-    setTimeout(function() {
-        splashScreen.style.opacity = "0";
-        splashScreen.style.transition = "opacity 0.5s ease";
+if (typeof load === "undefined") {
+    var load = 0
+} 
+
+if (load === 0) {
+    window.addEventListener("load", function() {
+        const splashScreen = document.getElementById("splash-screen");
         setTimeout(function() {
-            splashScreen.style.display = "none";
-        }, 500); // 0.5秒後に非表示
-    }, 1850); // 1.85秒後にフェードアウトを開始
-});
+            splashScreen.style.opacity = "0";
+            splashScreen.style.transition = "opacity 0.5s ease";
+            setTimeout(function() {
+                splashScreen.style.display = "none"; 
+            }, 500); 
+        }, 1850); 
+    });
+    load = 1
+}
+
+
+

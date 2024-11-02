4d03374e75855a14f128ea9a2a3993e0a5cf1ae3
FFT_dareka <152751544+FFTdareka@users.noreply.github.com>
2024-11-02 09:33:52 +0900

    Update style-1.js

--- style-1.js
+++ style-1.js
@@ -2457,16 +2457,18 @@ var obs_url = new MutationObserver(() => {
         open_pvm(uid, icon_n);
       });
     }
-    let cmt_icon_list = document.querySelectorAll(`.comment.clearfix`);
-    cmt_icon_list.forEach( el => {
-      let img = el.querySelector('.user');
-      let uid = img.getAttribute('data-uid');
-      let inum = img.getAttribute('data-img_no');
-      let cnum = el.querySelector('.m_no').innerText;
-      img.addEventListener('click', () => {
-        open_pvm(uid, inum, cnum);
+    if (!isPC) {
+      let cmt_icon_list = document.querySelectorAll(`.comment.clearfix`);
+      cmt_icon_list.forEach( el => {
+        let img = el.querySelector('.user');
+        let uid = img.getAttribute('data-uid');
+        let inum = img.getAttribute('data-img_no');
+        let cnum = el.querySelector('.m_no').innerText;
+        img.addEventListener('click', () => {
+          open_pvm(uid, inum, cnum);
+        });
       });
-    });
+    }
   }
 });
 
